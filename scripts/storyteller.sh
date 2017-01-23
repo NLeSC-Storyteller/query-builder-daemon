da008b14323516ae600f15b4461ea87bdbb600c0
maartenvm <m.vanmeersbergen@esciencecenter.nl>
2017-01-23 16:15:37 +0100

    updated update_queries.py to python3 and fixed linting errors. daemon now in working state.

--- scripts/storyteller.sh
+++ scripts/storyteller.sh
@@ -1,9 +1,9 @@
 JAVA_HOME="C:/ProgramData/Oracle/Java/javapath"
 PYTHON_HOME="C:/Users/Maarten/AppData/Local/Programs/Python/Python36-32"
-
 STORYTELLER_PATH="c:/Users/Maarten/workspace/StoryTeller"
+
 LIB="$STORYTELLER_PATH"/target
-DB="$STORYTELLER_PATH"/../query-builder-server/data/storyteller4.db
+DB="$STORYTELLER_PATH"/../query-builder-server/data/storyteller5.db
 RESOURCES="$( cd $STORYTELLER_PATH && cd .. && pwd)"/vua-resources
 
 SERVER="http://145.100.58.139:50053"
@@ -12,7 +12,8 @@ LIMIT="100"
 ID=$1
 QUERY=$2
 
-$JAVA_HOME/java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonMakeStoryFromTripleData  "$QUERY" --ks-limit $LIMIT --ks-service $SERVER --log --token-index "$STORYTELLER_PATH"/scripts/token.index.gz --eurovoc $RESOURCES/mapping_eurovoc_skos.label.concept.gz > $ID.json 2> error.log
+echo "starting query to KnowledgeStore"
+$JAVA_HOME/java -Xmx2000m -cp "$LIB/StoryTeller-v1.0-jar-with-dependencies.jar" vu.cltl.storyteller.json.JsonMakeStoryFromTripleData "$QUERY" --ks-limit $LIMIT --ks-service $SERVER --log --token-index "$STORYTELLER_PATH"/scripts/token.index.gz --eurovoc $RESOURCES/mapping_eurovoc_skos.label.concept.gz 1> $ID.json 2> log.err
 
-echo $DB
-$PYTHON_HOME/python update_queries.py $DB $ID $ID.json
+echo "starting update_queries"
+$PYTHON_HOME/python update_queries.py $DB $ID $ID.json 1>> log.out 2>> log.err
--- scripts/update_queries.py
+++ scripts/update_queries.py
@@ -1,54 +1,62 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+Update the SQLITE table queries with the returned JSON file from the Xenon command.
+"""
+
 import sqlite3 as lite
 import sys
 
-con = None
+def update_table(db_name, query_id, input_file_name):
+    """Update the SQLITE table queries with the returned JSON file from the Xenon command."""
+    con = None
+    try:
+        input_file = open(str(input_file_name), "rb")
+        ablob = input_file.read()
+        con = lite.connect(str(db_name))
+        cur = con.cursor()
+        stmt = "update queries set status=1, result=? where id=" + str(query_id)
+        print(stmt)
+        cur.execute(stmt, [lite.Binary(ablob)])
+        if cur.rowcount == 0:
+            stmt = "select count(status) from queries where id = " + str(query_id)
+            cur.execute(stmt)
+            rows = cur.fetchall()
+            if len(rows) == 1:
+                stmt = "update queries set status=2 where id=" + str(query_id)
+                cur.execute(stmt)
+                if cur.rowcount == 1:
+                    print("Update status to ERROR for query id " + str(query_id) + " succeeded")
+                else:
+                    print("Update status to ERROR for query id " + str(query_id) + " failed")
+            else:
+                err = "Query ID " + str(query_id) + " does not exist."
+                print(err)
+                stmt = """insert into queries (id, status, result)
+                        values(""" + str(query_id) + """, 2, ?)"""
+                cur.execute(stmt, [lite.Binary(err)])
+        else:
+            print("Update status to SUCCEEDED for query id " + str(query_id) + " succeeded")
 
-try:
-    input_file = open(str(sys.argv[3]), "rb")
-    ablob = input_file.read()
-    con = lite.connect(str(sys.argv[1]))
-    cur = con.cursor()   
-    stmt = "update queries set status=1, result=? where id=" + str (sys.argv[2])
-    print stmt
-    cur.execute(stmt, [lite.Binary(ablob)])
-    if (cur.rowcount == 0):
-        stmt = "select count(status) from queries where id = " + str (sys.argv[2])
+    except lite.Error as err:
+        stmt = "select status from queries where id = " + str(query_id)
         cur.execute(stmt)
         rows = cur.fetchall()
-        if (len(rows) == 1):
-            stmt = "update queries set status=2 where id=" + str (sys.argv[2])
-            cur.execute(stmt)
-            if (cur.rowcount == 1):
-                print "Update status to ERROR for query id " + str (sys.argv[2]) + " succeeded"
-            else:
-                print "Update status to ERROR for query id " + str (sys.argv[2]) + " failed"
+        if len(rows) == 1:
+            stmt = "update queries set status=2, result=? where id=" + str(query_id)
+            cur.execute(stmt, [lite.Binary(err.args[0])])
         else:
-            err = "Query ID " + str (sys.argv[2]) + " does not exist."
-            print err
-            stmt = "insert into queries (id, status, result) values(" + str (sys.argv[2]) + ", 2, ?)"
-            cur.execute(stmt, [lite.Binary(err)])
-    else:
-        print "Update status to SUCCEDDED for query id " + str (sys.argv[2]) + " succeeded"
+            stmt = "insert into queries (id, status, result) values(" + str(query_id) + ", 2, ?)"
+            cur.execute(stmt, [lite.Binary(err.args[0])])
+        print("Error %s:" % err.args[0])
+        sys.exit(1)
 
-except lite.Error, e:
-    stmt = "select status from queries where id = " + str (sys.argv[2])
-    cur.execute(stmt)
-    rows = cur.fetchall()
-    if (len(rows) == 1):
-        stmt = "update queries set status=2, result=? where id=" + str (sys.argv[2])
-        cur.execute(stmt, [lite.Binary(e.args[0])])
-    else:
-        stmt = "insert into queries (id, status, result) values(" + str (sys.argv[2]) + ", 2, ?)"
-        cur.execute(stmt, [lite.Binary(e.args[0])])
-    print "Error %s:" % e.args[0]
-    sys.exit(1)
+    finally:
+        if input_file:
+            input_file.close()
+        if con:
+            con.commit()
+            con.close()
 
-finally:
-    if input_file:
-        input_file.close()
-    if con:
-        con.commit()
-        con.close()
+update_table(sys.argv[1], sys.argv[2], sys.argv[3])
