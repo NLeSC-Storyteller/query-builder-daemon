57950df940e9c886217b5b4dcff607efa8b69b74
maartenvm <maartenvm@gmail.com>
2017-04-06 17:37:34 +0200

    Fixed typo in data dir.

--- scripts/clear_all_queries.sh
+++ scripts/clear_all_queries.sh
@@ -3,7 +3,7 @@
 DATA_DIR="/data"
 
 echo "starting clear_all_queries"
-cd DATA_DIR
+cd $DATA_DIR
 rm *.json
 
 python /src/query-builder-daemon/scripts/clear_all_queries.py $DATA_DIR/storyteller.db 1>> $DATA_DIR/log.out 2>> $DATA_DIR/log.err
